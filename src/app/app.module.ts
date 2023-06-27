226cb7e3239f4c9aa46b2571883bd9121d7a3949
MaximeCOMTE <97729276+MaximeCOMTE@users.noreply.github.com>
2023-06-27 08:04:26 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import {RouterLink} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterLink
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
